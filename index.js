4302062f4a1473463508b87db2a68c92249b08b6
Doug Dyson <dougdyson@gmail.com>
2020-08-27 23:12:27 +0000

    Add iss.js file. fetching IP via API working.

--- index.js
+++ index.js
@@ -5,4 +5,19 @@
 // 2. Pass the IP to get the geo coordinates (longitude/latitude)
 // 3. Use the geo coordinates to get when the ISS flies over
 // 
-// 
+// ============
+
+// index.js
+
+// The code below is temporary and can be commented out.
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
