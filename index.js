bfe523b8c3ab3511cf0455a77ec42a9941856941
Doug Dyson <dougdyson@gmail.com>
2020-08-27 22:57:07 +0000

    installed request library. added index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+// REQUIREMENTS
+// ============
+// Via API calls:
+// 1. Get the IP this program is running on
+// 2. Pass the IP to get the geo coordinates (longitude/latitude)
+// 3. Use the geo coordinates to get when the ISS flies over
+// 
+// 
